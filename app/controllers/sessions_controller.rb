cf044d4cde79d4df71584ce573a6992a4ced8ca1
Noah Schutte <noahschutte@gmail.com>
2016-04-20 19:37:42 -0400

    Add full question view page, complete user profile page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,22 @@
-get '/sessions/login' do
+get '/login' do
   @user = User.find_by(id: session[:user_id])
-  erb :'/sessions/login'
+  erb :'/sessions/new'
 end
 
-# post '/login' do
-#   if @user = User.find_by(email: params[:email])
-#     if @user.authenticate(params[:password])
-#       session[:user_id] = @user.id
-#       redirect "/users/#{ @user.id }"
-#     else
-#       redirect '/'
-#     end
-#   else
-#     redirect '/'
-#   end
-# end
+post '/login' do
+  if @user = User.find_by(username: params[:username])
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.id}"
+    else
+      redirect '/sessions/new'
+    end
+  else
+    redirect '/sessions/new'
+  end
+end
 
-# delete '/logout' do
-#   session.clear
-#   redirect '/'
-# end
+delete '/logout' do
+  session.clear
+  redirect '/'
+end
