d8118bfcdc673254e8f8d5725bb6bc59050540be
Theo Paul <theo.markkus.paul@gmail.com>
2016-04-20 18:20:39 -0400

    Add 2-4 random questions for each user

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,12 +17,41 @@ email4 = Faker::Internet.email(username4)
 username5 = Faker::Name.name
 email5 = Faker::Internet.email(username5)
 
-user1 = User.create!(username: username1, email: email1, password: Faker::Internet.password)
-user2 = User.create!(username: username2, email: email2, password: Faker::Internet.password)
-user3 = User.create!(username: username3, email: email3, password: Faker::Internet.password)
-user4 = User.create!(username: username4, email: email4, password: Faker::Internet.password)
-user5 = User.create!(username: username5, email: email5, password: Faker::Internet.password)
+user1 = User.create!(username: username1, email: email1, password: Faker::Internet.password) #has user_id = 1
+user2 = User.create!(username: username2, email: email2, password: Faker::Internet.password) #has user_id = 2
+user3 = User.create!(username: username3, email: email3, password: Faker::Internet.password) #has user_id = 3
+user4 = User.create!(username: username4, email: email4, password: Faker::Internet.password) #has user_id = 4
+user5 = User.create!(username: username5, email: email5, password: Faker::Internet.password) #has user_id = 5
+
 
 #Questions from User
+
+# generates random array of four words
+# each word in array is capitalised
+# Each index inside of the array is combine with space between each word
+
+def create_title
+  Faker::Lorem.words(4).map!{|word| word.capitalize}.join(' ')
+end
+
+def create_content
+  Faker::Lorem.paragraph
+end
+
+def generate_questions_for_users(num_of_ques, num_of_users)
+  num_of_ques.times do
+    num_of_users.each do |user|
+      Question.create!(title: create_title, content: create_content, user_id: user)
+    end
+  end
+end
+
+def rand_num
+  rand(2..4)
+end
+
+generate_questions_for_users(rand_num, [1, 2, 3, 4, 5])
+
+
 
 
