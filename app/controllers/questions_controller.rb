2cb3890a7ac374b001594978efca0a4b8f3b1078
Noah Schutte <noahschutte@gmail.com>
2016-04-20 20:19:34 -0400

    Create question formm

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,18 @@
-get '/question/index' do
+get '/questions/index' do
   @questions = Question.all
-  erb :'questions/index'
+  erb :'/questions/index'
 end
 
-get '/question/:id' do
+get '/questions/new' do
+  erb :'/questions/new'
+end
+
+post '/questions' do
+  binding.pry
+  @new_question = Question.create(params)
+end
+
+get '/questions/:id' do
   @question = Question.find_by(id: params[:id])
   erb :'/questions/show'
 end
