eba1299d18cb968350934fe456cadc4b245e9ae4
Ivy Vetor <ivyvetor@gmail.com>
2016-04-22 10:45:19 -0400

    fix vote on question, current shared master

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -6,7 +6,6 @@ get '/questions/:id/answers/new' do
 
   else
 
-
   end
     @question = Question.find_by(id: params[:id])
     erb :'/answers/new'
@@ -19,8 +18,6 @@ post '/questions/:id/answers' do
   @user = User.find_by(id: session[:user_id])
   @question = Question.find_by(id: params[:question_id])
   answer = Answer.new(content: params[:content], user_id: @user.id, question_id: @question.id)
-
-
 
 #   if answer.save
 #     redirect "/questions/#{@question.id}"
